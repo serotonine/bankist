25e602f3c699b4cc9a599a8289e841a15443e129
serotonine <ks.serotonine@gmail.com>
2024-10-16 17:23:46 +0200

    dev: Bankist account.

--- src/js/bank.js
+++ src/js/bank.js
@@ -1,85 +1,5 @@
 "use strict";
 
-// Store datas
-
-// // Data
-// const account1 = {
-//   owner: "Jonas Schmedtmann",
-//   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-//   interestRate: 1.2, // %
-//   pin: 11,
-
-//   movementsDates: [
-//     "2019-11-18T21:31:17.178Z",
-//     "2019-12-23T07:42:02.383Z",
-//     "2020-01-28T09:15:04.904Z",
-//     "2020-04-01T10:17:24.185Z",
-//     "2020-05-08T14:11:59.604Z",
-//     "2020-07-26T17:01:17.194Z",
-//     "2020-07-28T23:36:17.929Z",
-//     "2020-08-01T10:51:36.790Z",
-//   ],
-//   currency: "EUR",
-//   locale: "pt-PT", // de-DE
-// };
-
-// const account2 = {
-//   owner: "Jessica Davis",
-//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-//   interestRate: 1.5,
-//   pin: 22,
-
-//   movementsDates: [
-//     "2023-11-20T13:15:33.035Z",
-//     "2023-11-18T09:48:16.867Z",
-//     "2023-11-15T06:04:23.907Z",
-//     "2023-01-25T14:18:46.235Z",
-//     "2020-02-05T16:33:06.386Z",
-//     "2020-04-10T14:43:26.374Z",
-//     "2020-06-25T18:49:59.371Z",
-//     "2023-07-26T12:01:20.894Z",
-//   ],
-//   currency: "USD",
-//   locale: "en-US",
-// };
-
-// const account3 = {
-//   owner: "Steven Thomas Williams",
-//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
-//   interestRate: 0.7,
-//   pin: 33,
-//   movementsDates: [
-//     "2023-11-20T13:15:33.035Z",
-//     "2023-11-18T09:48:16.867Z",
-//     "2023-11-15T06:04:23.907Z",
-//     "2023-01-25T14:18:46.235Z",
-//     "2020-02-05T16:33:06.386Z",
-//     "2020-04-10T14:43:26.374Z",
-//     "2020-06-25T18:49:59.371Z",
-//     "2023-07-26T12:01:20.894Z",
-//   ],
-//   currency: "EUR",
-//   locale: "fr-BE",
-// };
-
-// const account4 = {
-//   owner: "Sarah Smith",
-//   movements: [430, 1000, 700, 50, 90],
-//   currency: "USD",
-//   locale: "en-US",
-//   interestRate: 1,
-//   pin: 44,
-//   movementsDates: [
-//     "2023-11-20T13:15:33.035Z",
-//     "2023-11-18T09:48:16.867Z",
-//     "2023-11-15T06:04:23.907Z",
-//     "2023-01-25T14:18:46.235Z",
-//     "2020-02-05T16:33:06.386Z",
-//   ],
-//   currency: "EUR",
-//   locale: "nl-BE",
-// };
-
 // Elements
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
@@ -107,26 +27,24 @@ const inputClosePin = document.querySelector(".form__input--pin");
 const selectTransferTo = document.querySelector(".form__select--to");
 
 /////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
 
-const currencies = new Map([
-  ["USD", "United States dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound sterling"],
-]);
-// TODO UPDATE CODE.
-const accounts = JSON.parse(datas);
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// See localStorage.js
+let accounts = JSON.parse(datas);
 // Current account.
 let currentAccount = {};
 let timer;
+
 /////////////////////////////////////////////////
+
 //empty movements container
 containerMovements.innerHTML = "";
 
+// UPDATE LOCAL STORAGE //
+const updateLocalStorage = function(){
+  localStorage.setItem('bankist', JSON.stringify(accounts));
+}
+
 // DISPLAY DATE //
-// DATE FUNCTIONS
 const displayDate = function (acc, d, hm = false) {
   const l = acc.locale;
   const options = {
@@ -175,9 +93,12 @@ const intlNb = (account, num) => {
 
 /// DISPLAY MVT FCT ///
 const displayMvt = function (acc, sort = false) {
+  // Reset container.
+  containerMovements.innerHTML = "";
   const mouvements = sort
     ? acc.movements.slice().sort((a, b) => a - b)
     : acc.movements;
+
   mouvements.forEach(function (value, index) {
     const type = value > 0 ? "deposit" : "withdrawal";
     const html = `<div class="movements__row">
@@ -200,7 +121,6 @@ const createUserName = function (accounts = []) {
       .split(" ")
       .map((value) => value.at(0))
       .join("");
-    console.log("user name", acc.username);
   });
 };
 // Store the current balance on each accounts.
@@ -224,7 +144,6 @@ const displayWithdrawal = function (account) {
       .filter((value) => value < 0)
       .reduce((acc, value) => acc + value, 0)
   );
-  // labelSumOut.textContent = `${withdrawal.toFixed(2)} €`;
   labelSumOut.textContent = `${intlNb(account, withdrawal)}`;
 };
 const displayInterest = function (account) {
@@ -259,14 +178,11 @@ const options = accounts.map(
   (acc) => `<option value="${acc.username}">${acc.owner}</option>`
 );
 selectTransferTo.insertAdjacentHTML("beforeend", options.join("\n"));
-// NO LOGIN
-// displaySummary(currentAccount);
-// containerApp.style.opacity = 100;
 
 // Log out timer //
 const startLogOutTimer = function () {
   // Set time (seconds).
-  let t = 60;
+  let t = 1800;
   // Call the timer every second.
   const timer = setInterval(function () {
     const min = `${Math.trunc(t / 60)}`.padStart(2, 0);
@@ -321,6 +237,7 @@ btnTransfer.addEventListener("click", function (e) {
   const receiverAcc = accounts.find(
     (acc) => acc.username === selectTransferTo.value
   );
+
   if (
     receiverAcc &&
     receiverAcc.owner !== currentAccount.owner &&
@@ -333,19 +250,19 @@ btnTransfer.addEventListener("click", function (e) {
     //
     console.log(`Transfert ready to be executed.`);
     // retrieve transfered amount from currentAccount.movements,
-    // set && display currentAccount withdrawal.
-    //TODO make function
+    // set currentAccount withdrawal.
     const now = new Date();
     currentAccount.movements.push(-amount);
     currentAccount.movementsDates.push(now.toISOString());
-    displaySummary(currentAccount);
-    // add amount to receiver
+    // Add amount to receiver account.
     receiverAcc.movements.push(amount);
     receiverAcc.movementsDates.push(now.toISOString());
-    // init input
+    // Reset input.
     inputTransferAmount.value = selectTransferTo.value = "";
-    // test if all is ok.
-    console.log(accounts);
+    // Display withdrawal.
+    displaySummary(currentAccount);
+    // Update localStorage;
+    updateLocalStorage();
   } else {
     console.error("Check your command something is wrong");
   }
@@ -368,10 +285,11 @@ btnLoan.addEventListener("click", function (e) {
     //Push date
     const now = new Date();
     currentAccount.movementsDates.push(now.toISOString());
-
     inputLoanAmount.value = "";
-    // setTimeout(callback function, delay, callback function arguments)
-    setTimeout(displaySummary, 2000, currentAccount);
+    // Update localStorage
+    updateLocalStorage();
+    // setTimeout(callback function, delay, callback function arguments).
+    setTimeout(displaySummary, 200, currentAccount);
   } else {
     console.log(`Deposit not granted : no deposit >= ${amount * 0.1}.`);
   }
@@ -388,8 +306,13 @@ btnClose.addEventListener("click", function (e) {
     const index = accounts.findIndex(function (acc) {
       return acc.username === inputCloseUsername.value;
     });
+    labelWelcome.textContent = `Bye bye, ${currentAccount.owner
+      .split(" ")
+      .at(0)}`;
     accounts.splice(index, 1);
     containerApp.style.opacity = 0;
+    // Update localStorage
+    updateLocalStorage();
   } else {
     console.error(`Wrong username or wrong pin.`);
   }
--- src/js/modal.js
+++ src/js/modal.js
@@ -4,6 +4,9 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".btn--close-modal");
 const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const modalForm = document.querySelector(".modal__form");
+// FORM & RESPONSE //
+const itemForm = document.querySelector(".modal__item--form");
+const itemResponse = document.querySelector(".modal__item--response");
 
 const openModal = function (e) {
   e.preventDefault();
@@ -14,6 +17,9 @@ const openModal = function (e) {
 const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+  itemResponse.classList.remove("active");
+  itemResponse.innerHTML = "";
+  itemForm.classList.remove("submitted");
 };
 btnsOpenModal.forEach((button) => button.addEventListener("click", openModal));
 
@@ -27,8 +33,7 @@ document.addEventListener("keydown", function (e) {
 });
 
 /// SEND DATAS ///
-const itemForm = document.querySelector(".modal__item--form");
-const itemResponse = document.querySelector(".modal__item--response");
+
 
 // Set Owner.
 const fixName = function (name) {
@@ -36,6 +41,10 @@ const fixName = function (name) {
   const lc = name.toLowerCase().trim();
   const capitalizeName = lc[0].toUpperCase() + lc.slice(1);
   return capitalizeName;
+};
+const getUserAccount = function (name = "") {
+  const split = name.toLowerCase().split(" ");
+  return split.map((c) => c[0]).join("");
 };
 const setOwner = function (first, last) {
   return `${fixName(first)} ${fixName(last)}`;
@@ -55,7 +64,7 @@ modalForm.addEventListener("submit", async (evt) => {
   console.log(formData);
   const user = {
     owner: setOwner(formData.first, formData.last),
-    movements: [formData.deposit],
+    movements: [+formData.deposit],
     interestRate: 1.5,
     pin: setPin(),
     movementsDates: [setNow()],
@@ -67,7 +76,20 @@ modalForm.addEventListener("submit", async (evt) => {
   localStorage.setItem("bankist", JSON.stringify(datas));
   // Reset form;
   modalForm.reset();
+
   // Display response mess.
+  const response = ` <img src='./src/img/logo-single.png' alt='bankist logo' />
+  <h2 class="modal__header">
+  Dear ${user.owner}<br/>your Bankist account is created!</h2>
+  <div><p>Your user name is <b>${getUserAccount(
+    user.owner
+  )}</b></p><p>Your code PIN is <b>${user.pin}</b></p></div>
+  
+<a class="btn btn--bank" href="bank.html">
+  Please visit your account&nbsp;&rightarrow;
+</a>`;
+  itemResponse.innerHTML = response;
   itemResponse.classList.add("active");
   itemForm.classList.add("submitted");
 });
+
